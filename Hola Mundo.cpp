056ae01b131c1f820c5b3bea08c3cfc48dfc08f7
Andres <andrescastr097@hotmail.com>
2019-09-09 22:10:28 -0600

    Ahora el hola munde tiene el getch();

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -1,12 +1,12 @@
 //primer programa en c++
 
 #include <iostream> // entraada y salida de datos
-
+#inlcude <conio.h>
 using namespace std; //imprimir
 
 int main (){
 	
 	cout<<"Hola Mundo :D"<<endl;  //endl es un enter,
-	
+	getch();
 	return 0;
 }
